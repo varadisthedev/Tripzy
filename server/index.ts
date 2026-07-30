a6cbe8227e2a9da01a9941faed32e387ffa358b8
varadisthedev <varadisthedev@gmail.com>
2026-07-30 22:12:26 +0530

    style(landing page): blurred image from hero section and changed app name

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+import chalk from "chalk";
+import morgan from "morgan";
+import helmet from "helmet";
+
+// middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+
+
+app.listen(process.env.PORT, async () => {
+  console.clear();
+  console.log(`Server is running on port ${process.env.PORT}`);
+
+
+});
